6083c07e913ffd9a64e0b19e5be8b21b07c36fdd
qcheeseborough <Qaiyim.Cheeseborough@gmail.com>
2020-01-01 19:33:43 -0500

    Added all buttons for fare search box.

--- index.php
+++ index.php
@@ -13,54 +13,103 @@
 
 <body>
     <!--    Navigation -->
-    <div class="w3-bar">
-        <a href="index.php" class="w3-bar-item w3-button w3-mobile">Home</a>
-        <a href="about.html" class="w3-bar-item w3-button w3-mobile">About</a>
-        <a href="contact.html" class="w3-bar-item w3-button w3-mobile">Contact</a>
+    <div class="w3-top">
+        <div class="w3-bar">
+            <a href="index.php" class="w3-bar-item w3-button w3-mobile w3-right">Home</a>
+            <a href="about.html" class="w3-bar-item w3-button w3-mobile w3-right">About</a>
+            <a href="contact.html" class="w3-bar-item w3-button w3-mobile w3-right">Contact</a>
+        </div>
     </div>
     <!--    Flight Search Container -->
-    <div class="w3-container">
-        <div class="w3-panel w3-green">
-            <h1>Choose Your Next Itinerary</h1>
-        </div>
-        <div class="w3-dropdown-click">
-            <button onclick="myFunction()" class="w3-button">Fare Type</button>
-            <div id="fareType" class="w3-dropdown-content w3-bar-block w3-card-4 w3-animate-zoom">
-                <a href="#" class="w3-bar-item w3-button">Round Trip</a>
-                <a href="#" class="w3-bar-item w3-button">One Way</a>
-                <a href="#" class="w3-bar-item w3-button">Multi-City</a> 
+    <div class="w3-display-container" style="height:300px;">
+        <div class="w3-display-topmiddle">
+            <div class="w3-container">
+                <div class="w3-panel w3-green">
+                    <h1>Choose Your Next Itinerary</h1>
+                </div>
             </div>
         </div>
-        <div class="w3-dropdown-click">
-            <button onclick="myFunction()" class="w3-button">Fare Class</button>
-            <div id="fareClass" class="w3-dropdown-content w3-bar-block w3-card-4 w3-animate-zoom">
-                <a href="#" class="w3-bar-item w3-button">First Class</a>
-                <a href="#" class="w3-bar-item w3-button">Business Class</a>
-                <a href="#" class="w3-bar-item w3-button">Premium Economy</a> 
+        <div class="w3-display-left">
+            <div class="w3-dropdown-click">
+                <button onclick="myFunction()" class="w3-button">Fare Type</button>
+                <div id="fareType" class="w3-dropdown-content w3-bar-block w3-card-4 w3-animate-zoom">
+                    <a href="#" class="w3-bar-item w3-button">Round Trip</a>
+                    <a href="#" class="w3-bar-item w3-button">One Way</a>
+                    <a href="#" class="w3-bar-item w3-button">Multi-City</a>
+                </div>
             </div>
         </div>
-        <div class="w3-dropdown-click">
-            <button onclick="myFunction()" class="w3-button">Passengers</button>
-            <div id="travelers" class="w3-dropdown-content w3-bar-block w3-card-4 w3-animate-zoom">
-                <a href="#" class="w3-bar-item w3-button">1</a>
-                <a href="#" class="w3-bar-item w3-button">2</a>
-                <a href="#" class="w3-bar-item w3-button">3</a> 
-                <a href="#" class="w3-bar-item w3-button">4</a> 
-                <a href="#" class="w3-bar-item w3-button">5</a> 
-                <a href="#" class="w3-bar-item w3-button">6</a> 
-                <a href="#" class="w3-bar-item w3-button">7</a> 
-                <a href="#" class="w3-bar-item w3-button">8</a> 
-                <a href="#" class="w3-bar-item w3-button">9</a> 
+        <div class="w3-display-right">
+            <div class="w3-dropdown-click">
+                <button onclick="myFunction3()" class="w3-button">Passengers</button>
+                <div id="travelers" class="w3-dropdown-content w3-bar-block w3-card-4 w3-animate-zoom">
+                    <a href="#" class="w3-bar-item w3-button">1</a>
+                    <a href="#" class="w3-bar-item w3-button">2</a>
+                    <a href="#" class="w3-bar-item w3-button">3</a>
+                    <a href="#" class="w3-bar-item w3-button">4</a>
+                    <a href="#" class="w3-bar-item w3-button">5</a>
+                    <a href="#" class="w3-bar-item w3-button">6</a>
+                    <a href="#" class="w3-bar-item w3-button">7</a>
+                    <a href="#" class="w3-bar-item w3-button">8</a>
+                    <a href="#" class="w3-bar-item w3-button">9</a>
+                </div>
             </div>
         </div>
+        <div class="w3-display-middle">
+            <div class="w3-dropdown-click">
+                <button onclick="myFunction2()" class="w3-button">Fare Class</button>
+                <div id="fareClass" class="w3-dropdown-content w3-bar-block w3-card-4 w3-animate-zoom">
+                    <a href="#" class="w3-bar-item w3-button">First Class</a>
+                    <a href="#" class="w3-bar-item w3-button">Business Class</a>
+                    <a href="#" class="w3-bar-item w3-button">Premium Economy</a>
+                </div>
+            </div>
+        </div>
+        <div class="w3-display-bottomleft">
+            <label>Origin</label>
+            <input class="w3-input" type="text" placeholder="Enter origin city or airport code">
+        </div>
+        <div class="w3-display-bottommiddle">
+            <label>Destination</label>
+            <input class="w3-input" type="text" placeholder="Enter destination city or aiport code">
+        </div>
+        <div class="w3-display-bottomright">
+            <label>Departure Date</label>
+            <input class="w3-input" type="date">
 
+            <label>Return Date</label>
+            <input class="w3-input" type="date">
+        </div>
 
     </div>
+
+    <div class="w3-container">
+        <input class="w3-button w3-green w3-round-xxlarge" type="button" value="Search Now">
+    </div>
+
 
 
     <script>
         function myFunction() {
+            var x = document.getElementById("fareType");
+            if (x.className.indexOf("w3-show") == -1) {
+                x.className += " w3-show";
+            } else {
+                x.className = x.className.replace(" w3-show", "");
+            }
+        }
+
+        function myFunction2() {
             var x = document.getElementById("fareClass");
+            if (x.className.indexOf("w3-show") == -1) {
+                x.className += " w3-show";
+            } else {
+                x.className = x.className.replace(" w3-show", "");
+            }
+        }
+
+        function myFunction3() {
+            var x = document.getElementById("travelers");
             if (x.className.indexOf("w3-show") == -1) {
                 x.className += " w3-show";
             } else {
