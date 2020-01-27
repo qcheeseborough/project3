68b684a245710ebe8442e698543facb59c694a1a
qcheeseborough <Qaiyim.Cheeseborough@gmail.com>
2020-01-26 20:53:31 -0500

    Updated search form container formatting.  Still need to fix Departure Date and Return Date formatting as well as add API calls to Origin, Destination, and Search Now buttons.

--- index.php
+++ index.php
@@ -20,96 +20,77 @@
             <a href="contact.html" class="w3-bar-item w3-button w3-mobile w3-right">Contact</a>
         </div>
     </div>
-    <!--    Flight Search Container -->
+    <!--    Flight Search Banner Container -->
     <div class="w3-display-container" style="height:300px;">
-        <div class="w3-display-topmiddle">
+        <div class="w3-display-middle">
             <div class="w3-container">
                 <div class="w3-panel w3-green">
                     <h1>Choose Your Next Itinerary</h1>
                 </div>
             </div>
         </div>
+    </div>
+<!--    Flight Search Form Container -->
+    <div class="w3-display-container" style="height:450px;">
         <form action="/index.php">
-        <div class="w3-display-left">
+            <div class="w3-display-topleft">
+                <div class="w3-panel w3-green">
+                    <p>Fare Type</p>
+                </div>
                 <select name="Fare Type">
                     <option value="roundtrip">Round Trip</option>
                     <option value="oneway">One Way</option>
                     <option value="multicity">Multi-City</option>
                 </select>
-        </div>
-        <div class="w3-display-right">
-            <select name="Passengers">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-            </select>
-        </div>
-        <div class="w3-display-middle">
-            <select name="Fare Class">
-                <option value="firstClass">First Class</option>
-                <option value="businessClass">Business Class</option>
-                <option value="premiumEcon">Premium Economy</option>
-            </select>
-        </div>
-        <div class="w3-display-bottomleft">
-            <label>Origin</label>
-            <input class="w3-input" type="text" placeholder="Enter origin city or airport code">
-        </div>
-        <div class="w3-display-bottommiddle">
-            <label>Destination</label>
-            <input class="w3-input" type="text" placeholder="Enter destination city or aiport code">
-        </div>
-        <div class="w3-display-bottomright">
-            <label>Departure Date</label>
-            <input class="w3-input" type="date">
+            </div>
+            <div class="w3-display-topright">
+                <div class="w3-panel w3-green">
+                    <p>Passengers</p>
+                </div>
+                <select name="Passengers">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                </select>
+            </div>
+            <div class="w3-display-topmiddle">
+                <div class="w3-panel w3-green">
+                    <p>Fare Class</p>
+                </div>
+                <select name="Fare Class">
+                    <option value="firstClass">First Class</option>
+                    <option value="businessClass">Business Class</option>
+                    <option value="premiumEcon">Premium Economy</option>
+                </select>
+            </div>
+            <div class="w3-display-left">
+                <label>Origin</label>
+                <input class="w3-input" type="text" placeholder="Enter origin city or airport code">
+            </div>
+            <div class="w3-display-middle">
+                <label>Destination</label>
+                <input class="w3-input" type="text" placeholder="Enter destination city or aiport code">
+            </div>
+            <div class="w3-display-right">
+                <label>Departure Date</label>
+                <input class="w3-input" type="date">
 
-            <label>Return Date</label>
-            <input class="w3-input" type="date">
-        </div>
+                <label>Return Date</label>
+                <input id="rtndate" class="w3-input" type="date">
+            </div>
 
+            <div class="w3-container w3-center w3-margin w3-display-bottommiddle">
+                <input class="w3-button w3-green w3-round-xxlarge" type="button" value="Search Now">
+            </div>
+        </form>
     </div>
 
-    <div class="w3-container w3-center w3-margin">
-        <input class="w3-button w3-green w3-round-xxlarge" type="button" value="Search Now">
-    </div>
-</form>
-
-
-    <script>
-        function myFunction() {
-            var x = document.getElementById("fareType");
-            if (x.className.indexOf("w3-show") == -1) {
-                x.className += " w3-show";
-            } else {
-                x.className = x.className.replace(" w3-show", "");
-            }
-        }
-
-        function myFunction2() {
-            var x = document.getElementById("fareClass");
-            if (x.className.indexOf("w3-show") == -1) {
-                x.className += " w3-show";
-            } else {
-                x.className = x.className.replace(" w3-show", "");
-            }
-        }
-
-        function myFunction3() {
-            var x = document.getElementById("travelers");
-            if (x.className.indexOf("w3-show") == -1) {
-                x.className += " w3-show";
-            } else {
-                x.className = x.className.replace(" w3-show", "");
-            }
-        }
-
-    </script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="js/main.js"></script>
 </body>
